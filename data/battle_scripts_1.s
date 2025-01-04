0e447d5a1eedb999e1865136ff8c8389cdb556cc
Southport <59145139+ynjtrademark@users.noreply.github.com>
2025-01-04 01:45:16 -0500

    Update to Centra City and misc script and code changes

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -5815,7 +5815,6 @@ BattleScript_TryLearnMoveLoop::
 BattleScript_AskToLearnMove::
 	buffermovetolearn
 	printstring STRINGID_TRYTOLEARNMOVE1
-	printstring STRINGID_TRYTOLEARNMOVE2
 	printstring STRINGID_TRYTOLEARNMOVE3
 	waitstate
 	setbyte sLEARNMOVE_STATE, 0
